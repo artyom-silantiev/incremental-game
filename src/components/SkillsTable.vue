415231b0b2ce79372d67beabfc0049b18744d66e
Artyom Silantiev <artyom.silantiev@gmail.com>
2023-03-19 09:14:37 +0300

    refact skills table

---
+++ src/components/SkillsTable.vue
@@ -0,0 +1,82 @@
+<script lang="ts">
+export default {
+  name: 'SkillsTable',
+};
+</script>
+
+<script setup lang="ts">
+import { QTableColumn } from 'quasar';
+import { Skill } from 'src/types/skill';
+
+export interface Props {
+  skills: Skill[];
+  subTable?: boolean;
+}
+const props = withDefaults(defineProps<Props>(), {
+  subTable: false,
+});
+
+const columns = [
+  { name: 'name', field: 'name', label: 'Name', align: 'left' },
+  { name: 'level', field: 'level', label: 'Level', align: 'left' },
+  { name: 'xp', field: 'xp', label: 'Xp', align: 'left' },
+  { name: 'xpPlus', field: 'xpPlus', label: 'XpPlus', align: 'left' },
+  { name: 'actions', field: 'actions', label: 'Actions', align: 'left' },
+] as QTableColumn[];
+</script>
+
+<template>
+  <q-table
+    :title="props.subTable ? undefined : 'Skills'"
+    :rows="skills.filter((x) => x.enabled)"
+    :columns="columns"
+    row-key="index"
+    :dense="props.subTable"
+    hide-bottom
+  >
+    <template v-slot:header="props">
+      <q-tr :props="props">
+        <q-th v-for="col in props.cols" :key="col.name" :props="props">
+          {{ col.label }}
+        </q-th>
+      </q-tr>
+    </template>
+
+    <template v-slot:body="props">
+      <q-tr :props="props" :key="props.row.type.sysName">
+        <q-td key="name">
+          {{ props.row?.type.name }}
+        </q-td>
+        <q-td key="level">
+          {{ props.row?.level }}
+        </q-td>
+        <q-td key="xp"> {{ props.row?.xp }}/{{ props.row?.xpNeed }} </q-td>
+        <q-td key="xpPlus">
+          {{ props.row?.xpPlus.mul(props.row.xpRate) }}
+        </q-td>
+        <q-td key="actions"> ... </q-td>
+      </q-tr>
+      <q-tr
+        :props="props"
+        :key="`e_${props.row.index}`"
+        class="q-virtual-scroll--with-prev"
+      >
+        <q-td colspan="100%">
+          <q-list dense>
+            <q-item>
+              {{ props.row?.type.description }}
+            </q-item>
+            <q-expansion-item
+              v-if="props.row?.subSkills.filter((x: Skill) => x.enabled).length"
+              label="Sub skills"
+              expand-separator
+              dense
+            >
+              <SkillsTable :skills="props.row?.subSkills" :subTable="true" />
+            </q-expansion-item>
+          </q-list>
+        </q-td>
+      </q-tr>
+    </template>
+  </q-table>
+</template>
--- src/pages/game/SkillsPage.vue
+++ src/pages/game/SkillsPage.vue
@@ -1,124 +1,16 @@
 <script setup lang="ts">
 import { useGameStore } from 'src/stores/game';
+import SkillsTable from 'src/components/SkillsTable.vue';
 import { Skill } from 'src/types/skill';
-
 const gameStore = useGameStore();
 const game = gameStore.game;
-const skills = game.skills.mainSkills;
-
-const columns = [
-  { name: 'name', field: 'name', label: 'Name', align: 'left' },
-  { name: 'level', field: 'level', label: 'Level', align: 'left' },
-  { name: 'xp', field: 'xp', label: 'Xp', align: 'left' },
-  { name: 'xpPlus', field: 'xpPlus', label: 'XpPlus', align: 'left' },
-  { name: 'actions', field: 'actions', label: 'Actions', align: 'left' },
-];
+const skills = game.skills.mainSkills as Skill[];
 </script>
 
 <template>
   <q-page class="skills-page">
     <div class="skills-table q-mt-md q-pa-md q-gutter-sm">
-      <q-table
-        title="Skills"
-        :rows="skills.filter((x) => x.enabled)"
-        :columns="columns"
-        row-key="index"
-        hide-bottom
-      >
-        <template v-slot:header="props">
-          <q-tr :props="props">
-            <q-th v-for="col in props.cols" :key="col.name" :props="props">
-              {{ col.label }}
-            </q-th>
-          </q-tr>
-        </template>
-
-        <template v-slot:body="props">
-          <q-tr :props="props" :key="`m_${props.row.index}`">
-            <q-td key="name" :props="props">
-              {{ props.row?.type.name }}
-            </q-td>
-            <q-td key="level" :props="props">
-              {{ props.row?.level }}
-            </q-td>
-            <q-td key="xp" :props="props">
-              {{ props.row?.xp }}/{{ props.row?.xpNeed }}
-            </q-td>
-            <q-td key="xpPlus" :props="props">
-              {{ props.row?.xpPlus.mul(props.row.xpRate) }}
-            </q-td>
-            <q-td key="actions" :props="props"> ... </q-td>
-          </q-tr>
-          <q-tr
-            :props="props"
-            :key="`e_${props.row.index}`"
-            class="q-virtual-scroll--with-prev"
-          >
-            <q-td colspan="100%">
-              <q-list dense>
-                <q-item>
-                  {{ props.row?.type.description }}
-                </q-item>
-                <q-expansion-item
-                  v-if="props.row?.subSkills.filter((x: Skill) => x.enabled).length"
-                  label="Sub skills"
-                  expand-separator
-                  dense
-                >
-                  <q-table
-                    :rows="props.row.subSkills.filter((x: Skill) => x.enabled)"
-                    :columns="columns"
-                    row-key="index"
-                    dense
-                    hide-bottom
-                  >
-                    <template v-slot:header="props">
-                      <q-tr :props="props">
-                        <q-th
-                          v-for="col in props.cols"
-                          :key="col.name"
-                          :props="props"
-                        >
-                          {{ col.label }}
-                        </q-th>
-                      </q-tr>
-                    </template>
-
-                    <template v-slot:body="props">
-                      <q-tr :props="props" :key="`m_${props.row.index}`">
-                        <q-td key="name" :props="props">
-                          {{ props.row?.type.name }}
-                        </q-td>
-                        <q-td key="level" :props="props">
-                          {{ props.row?.level }}
-                        </q-td>
-                        <q-td key="xp" :props="props">
-                          {{ props.row?.xp }}/{{ props.row?.xpNeed }}
-                        </q-td>
-                        <q-td key="xpPlus" :props="props">
-                          {{ props.row?.xpPlus.mul(props.row.xpRate) }}
-                        </q-td>
-                        <q-td key="actions" :props="props"> ... </q-td>
-                      </q-tr>
-                      <q-tr
-                        :props="props"
-                        :key="`e_${props.row.index}`"
-                        class="q-virtual-scroll--with-prev"
-                      >
-                        <q-td colspan="100%">
-                          <q-list dense>
-                            <q-item>{{ props.row?.type.description }}</q-item>
-                          </q-list>
-                        </q-td>
-                      </q-tr>
-                    </template>
-                  </q-table>
-                </q-expansion-item>
-              </q-list>
-            </q-td>
-          </q-tr>
-        </template>
-      </q-table>
+      <SkillsTable :skills="skills" />
     </div>
   </q-page>
 </template>
